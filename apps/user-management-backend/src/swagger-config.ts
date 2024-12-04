c48fe58cf36f83539da0092f603d6745bcd70bcb
ingale12345 <ingaleprajawal@gmail.com>
2024-12-05 01:09:20 +0530

    nx setup in progress

---
+++ apps/user-management-backend/src/swagger-config.ts
@@ -0,0 +1,32 @@
+import { usersSchema } from './services/users/users.schema'
+import { LoginDetailsSchema } from './Types'
+
+export const swaggerConfig = {
+  definition: {
+    openapi: '3.0.0', // OpenAPI version
+    info: {
+      title: 'FeathersJS API', // API Title
+      version: '1.0.0', // API version
+      description: 'A FeathersJS API documentation using Swagger'
+    },
+    servers: [
+      {
+        url: 'http://localhost:3030' // Your API base URL
+      }
+    ],
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          type: 'http',
+          scheme: 'bearer'
+        }
+      },
+      schemas: {
+        Users: usersSchema,
+        LoginDetails: LoginDetailsSchema
+      }
+    },
+    security: [{ BearerAuth: [] }]
+  },
+  apis: ['./src/services/**/*.ts'] // Path to your TypeScript service files
+}
